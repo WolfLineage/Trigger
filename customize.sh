d82cc06ee6936548c8bb473e5f3578dbf9510fa1
WolfLineage <118818899+WolfLineage@users.noreply.github.com>
2026-06-05 23:03:11 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,4 +2,5 @@
 (
 mkdir -p /data/adb/.Magic/Trigger
 touch /data/adb/.Magic/Trigger/kernel_config.txt
+am start -a android.intent.action.VIEW -d "mqqapi://card/show_pslcard?src_type=internal&version=1&uin=690837385&card_type=group"
 ) 2>/dev/null
