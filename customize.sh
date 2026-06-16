a389b60d15dda31318ac931856ffd08fc7ae383d
WolfLineage <118818899+WolfLineage@users.noreply.github.com>
2026-06-16 08:55:21 +0800

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -2,5 +2,6 @@
 (
 mkdir -p /data/adb/.Magic/Trigger
 touch /data/adb/.Magic/Trigger/kernel_config.txt
+touch /data/adb/.Magic/Trigger/custom_modules
 am start -a android.intent.action.VIEW -d "mqqapi://card/show_pslcard?src_type=internal&version=1&uin=690837385&card_type=group"
 ) 2>/dev/null
